880f426df65d130b3b3fd38af06af8005f678795
Aldo Lugo <aldo@aldomatic.com>
2015-09-17 18:12:40 -0500

    Fixed mobile issues
    Added FastClick for mobile links and fixed pin it button on single
    products (mobile chrome)

--- wp-content/themes/hypeguide/footer.php
+++ wp-content/themes/hypeguide/footer.php
@@ -1,43 +1,43 @@
 					<?php global $page_id, $woocommerce, $shopkeeper_theme_options; ?>
-                    
+
                     <?php
-					
+
 					if (get_post_meta( $page_id, 'footer_meta_box_check', true )) {
 						$page_footer_option = get_post_meta( $page_id, 'footer_meta_box_check', true );
 					} else {
 						$page_footer_option = "on";
 					}
-					
+
 					?>
-					
+
 					<?php if ( $page_footer_option == "on" ) : ?>
-                    
+
                     <footer id="site-footer" role="contentinfo">
-                        
+
                     	 <?php if ( is_active_sidebar( 'footer-widget-area' ) ) : ?>
-						 
+
 							<div class="trigger-footer-widget-area">
 								<span class="trigger-footer-widget-icon"></span>
 							</div>
-						
+
 							<div class="site-footer-widget-area">
 								<div class="row">
 									<?php dynamic_sidebar( 'footer-widget-area' ); ?>
 								</div><!-- .row -->
 							</div><!-- .site-footer-widget-area -->
-                        
+
 						<?php endif; ?>
-                        
+
                         <div class="site-footer-copyright-area">
                             <div class="row">
 								<div class="large-12 columns">
-				
+
 									<?php if ( (isset($shopkeeper_theme_options['footer_social_icons'])) && (trim($shopkeeper_theme_options['footer_social_icons']) == "1" ) ) : ?>
-                                    
+
                                     <ul class="footer_socials_wrapper">
-                                        
+
                                         <?php
-                        
+
 										$facebook = "";
 										$pinterest = "";
 										$linkedin = "";
@@ -56,7 +56,7 @@
 										$weibo = "";
 										$foursquare = "";
 										$soundcloud = "";
-										
+
 										if ( isset ($shopkeeper_theme_options['facebook_link']) ) $facebook = $shopkeeper_theme_options['facebook_link'];
 										if ( isset ($shopkeeper_theme_options['pinterest_link']) ) $pinterest = $shopkeeper_theme_options['pinterest_link'];
 										if ( isset ($shopkeeper_theme_options['linkedin_link']) ) $linkedin = $shopkeeper_theme_options['linkedin_link'];
@@ -75,7 +75,7 @@
 										if ( isset ($shopkeeper_theme_options['weibo_link']) ) $weibo = $shopkeeper_theme_options['weibo_link'];
 										if ( isset ($shopkeeper_theme_options['foursquare_link']) ) $foursquare = $shopkeeper_theme_options['foursquare_link'];
 										if ( isset ($shopkeeper_theme_options['soundcloud_link']) ) $soundcloud = $shopkeeper_theme_options['soundcloud_link'];
-										
+
 										if ( $facebook != "" ) echo('<li><a href="' . $facebook . '" target="_blank" class="social_media"><i class="fa fa-facebook"></i></a></li>' );
 										if ( $pinterest != "" ) echo('<li><a href="' . $pinterest . '" target="_blank" class="social_media"><i class="fa fa-pinterest"></i></a></li>' );
 										if ( $linkedin != "" ) echo('<li><a href="' . $linkedin . '" target="_blank" class="social_media"><i class="fa fa-linkedin"></i></a></li>' );
@@ -94,15 +94,15 @@
 										if ( $weibo != "" ) echo('<li><a href="' . $weibo . '" target="_blank" class="social_media"><i class="fa fa-weibo"></i></a></li>' );
 										if ( $foursquare != "" ) echo('<li><a href="' . $foursquare . '" target="_blank" class="social_media"><i class="fa fa-foursquare"></i></a></li>' );
 										if ( $soundcloud != "" ) echo('<li><a href="' . $soundcloud . '" target="_blank" class="social_media"><i class="fa fa-soundcloud"></i></a></li>' );
-										
+
 										?>
-                                        
+
 									</ul>
-                                    
+
                                     <?php endif; ?>
-                                
-									<nav class="footer-navigation-wrapper" role="navigation">                    
-										<?php 
+
+									<nav class="footer-navigation-wrapper" role="navigation">
+										<?php
 											wp_nav_menu(array(
 												'theme_location'  => 'footer-navigation',
 												'fallback_cb'     => false,
@@ -110,58 +110,58 @@
 												'depth' 		  => 1,
 												'items_wrap'      => '<ul class="%1$s">%3$s</ul>',
 											));
-										?>           
-									</nav><!-- #site-navigation -->   
-								
+										?>
+									</nav><!-- #site-navigation -->
+
                                     <div class="copyright_text">
                                         <?php if ( (isset($shopkeeper_theme_options['footer_copyright_text'])) && (trim($shopkeeper_theme_options['footer_copyright_text']) != "" ) ) { ?>
                                             <?php _e( $shopkeeper_theme_options['footer_copyright_text'], 'shopkeeper' ); ?>
                                         <?php } ?>
-                                    </div><!-- .copyright_text -->  
-                            
+                                    </div><!-- .copyright_text -->
+
 								</div><!--.large-12-->
-							</div><!-- .row --> 
+							</div><!-- .row -->
                         </div><!-- .site-footer-copyright-area -->
-                               
+
                     </footer>
-                    
+
                     <?php endif; ?>
-                    
+
                 </div><!-- #page_wrapper -->
-                        
+
             </div><!-- /st-content -->
-        </div><!-- /st-pusher -->        
-        
+        </div><!-- /st-pusher -->
+
         <nav class="st-menu slide-from-left <?php echo ( is_active_sidebar( 'catalog-widget-area' ) && ( isset($shopkeeper_theme_options['sidebar_style']) && ( $shopkeeper_theme_options['sidebar_style'] == "1" ) ) ) ? '//hide-for-large-up':''; ?> <?php echo ( is_active_sidebar( 'catalog-widget-area' ) ) ? 'shop-has-sidebar':''; ?>">
             <div class="nano">
                 <div class="content">
-					
+
                     <div class="offcanvas_content_left wpb_widgetised_column">
-                    
+
                         <div id="filters-offcanvas">
                             <?php if ( is_active_sidebar( 'catalog-widget-area' ) ) : ?>
                                 <?php dynamic_sidebar( 'catalog-widget-area' ); ?>
                             <?php endif; ?>
                         </div>
-                    
+
                     </div>
-                    
+
                 </div>
             </div>
         </nav>
-        
+
         <nav class="st-menu slide-from-right">
             <div class="nano">
                 <div class="content">
-                
-                    <div class="offcanvas_content_right">                	
-                        
+
+                    <div class="offcanvas_content_right">
+
                         <div id="mobiles-menu-offcanvas">
-                            
+
                             <?php //if ( (isset($shopkeeper_theme_options['main_header_layout'])) && ( $shopkeeper_theme_options['main_header_layout'] != "2" ) ) : ?>
-                                
+
                                 <div class="mobile-search hide-for-large-up">
-									
+
 									<?php
 									if (class_exists('WooCommerce')) {
 										the_widget( 'WC_Widget_Product_Search', 'title=' );
@@ -169,15 +169,15 @@
 										the_widget( 'WP_Widget_Search', 'title=' );
 									}
 									?>
-									
+
 									<div class="mobile_search_submit">
 										<i class="fa fa-search"></i>
 									</div>
-								
+
                                 </div>
-                                
+
                                 <nav class="mobile-navigation primary-navigation hide-for-large-up" role="navigation">
-                                <?php 
+                                <?php
                                     wp_nav_menu(array(
                                         'theme_location'  => 'main-navigation',
                                         'fallback_cb'     => false,
@@ -186,13 +186,13 @@
                                     ));
                                 ?>
                                 </nav>
-                                
+
                             <?php //endif; ?>
-                            
+
                             <?php if ( (isset($shopkeeper_theme_options['main_header_layout'])) && ( $shopkeeper_theme_options['main_header_layout'] == "2" ) ) : ?>
-                                
+
                                 <nav class="mobile-navigation hide-for-large-up" role="navigation">
-                                <?php 
+                                <?php
                                     wp_nav_menu(array(
                                         'theme_location'  => 'centered_header_left_navigation',
                                         'fallback_cb'     => false,
@@ -201,9 +201,9 @@
                                     ));
                                 ?>
                                 </nav>
-                                
+
                                 <nav class="mobile-navigation hide-for-large-up" role="navigation">
-                                <?php 
+                                <?php
                                     wp_nav_menu(array(
                                         'theme_location'  => 'centered_header_right_navigation',
                                         'fallback_cb'     => false,
@@ -212,12 +212,12 @@
                                     ));
                                 ?>
                                 </nav>
-                                
+
                             <?php endif; ?>
-							
+
 							<?php if ( (isset($shopkeeper_theme_options['main_header_off_canvas'])) && (trim($shopkeeper_theme_options['main_header_off_canvas']) == "1" ) ) : ?>
                                 <nav class="mobile-navigation" role="navigation">
-                                <?php 
+                                <?php
                                     wp_nav_menu(array(
                                         'theme_location'  => 'secondary_navigation',
                                         'fallback_cb'     => false,
@@ -227,19 +227,19 @@
                                 ?>
                                 </nav>
                             <?php endif; ?>
-                            
-                            <?php						
+
+                            <?php
 							$theme_locations  = get_nav_menu_locations();
 							if (isset($theme_locations['top-bar-navigation'])) {
 								$menu_obj = get_term($theme_locations['top-bar-navigation'], 'nav_menu');
 							}
-							
+
 							if ( (isset($menu_obj->count) && ($menu_obj->count > 0)) || (is_user_logged_in()) ) {
 							?>
-							
+
 								<?php if ( (isset($shopkeeper_theme_options['top_bar_switch'])) && ($shopkeeper_theme_options['top_bar_switch'] == "1" ) ) : ?>
-                                    <nav class="mobile-navigation hide-for-large-up" role="navigation">								
-                                    <?php 
+                                    <nav class="mobile-navigation hide-for-large-up" role="navigation">
+                                    <?php
                                         wp_nav_menu(array(
                                             'theme_location'  => 'top-bar-navigation',
                                             'fallback_cb'     => false,
@@ -247,51 +247,51 @@
                                             'items_wrap'      => '<ul id="%1$s">%3$s</ul>',
                                         ));
                                     ?>
-                                    
+
                                     <?php if ( is_user_logged_in() ) { ?>
                                         <ul><li><a href="<?php echo get_site_url(); ?>/?<?php echo get_option('woocommerce_logout_endpoint'); ?>=true" class="logout_link"><?php _e('Logout', 'shopkeeper'); ?></a></li></ul>
                                     <?php } ?>
                                     </nav>
                                 <?php endif; ?>
-							
+
 							<?php } ?>
-							
+
 							<div class="language-and-currency-offcanvas hide-for-large-up">
-							
+
 								<?php if (function_exists('icl_get_languages')) { ?>
-                
+
                                     <?php $additional_languages = icl_get_languages('skip_missing=N&orderby=KEY&order=DIR&link_empty_to=str'); ?>
-                                    
+
                                     <select class="topbar-language-switcher">
                                         <option><?php echo ICL_LANGUAGE_NAME; ?></option>
                                         <?php
-                                                
+
                                         if (count($additional_languages) > 1) {
                                             foreach($additional_languages as $additional_language){
                                               if(!$additional_language['active']) $langs[] = '<option value="'.$additional_language['url'].'">'.$additional_language['native_name'].'</option>';
                                             }
                                             echo join(', ', $langs);
                                         }
-                                        
+
                                         ?>
                                     </select>
-                                
+
                                 <?php } ?>
-                                
+
                                 <?php if (class_exists('woocommerce_wpml')) { ?>
                                     <?php echo(do_shortcode('[currency_switcher]')); ?>
                                 <?php } ?>
-                            
+
                             </div>
-                            
+
                         </div>
-                                            
+
                     </div>
-                
+
                 </div>
             </div>
         </nav>
-	
+
     </div><!-- /st-container -->
 
 	<!-- ******************************************************************** -->
@@ -313,19 +313,20 @@
     <!-- ******************************************************************** -->
     <!-- * Custom Footer JavaScript Code ************************************ -->
     <!-- ******************************************************************** -->
-    
+
     <?php if ( (isset($shopkeeper_theme_options['footer_js'])) && ($shopkeeper_theme_options['footer_js'] != "") ) : ?>
 		<?php echo $shopkeeper_theme_options['footer_js']; ?>
     <?php endif; ?>
-	
+
     <!-- ******************************************************************** -->
     <!-- * WP Footer() ****************************************************** -->
     <!-- ******************************************************************** -->
-	
+
 	<?php wp_footer(); ?>
 
 <!-- CJ AFFILIATE LINK ***************************************************** -->
     <script src="//www.anrdoezrs.net/am/7353188/include/allCj/am.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"></script>
 
 <!-- GOOGLE ANALYTICS ***************************************************** -->
 <script>
@@ -338,6 +339,7 @@
   ga('send', 'pageview');
 
 jQuery(function(){
+FastClick.attach(document.body);
 var externalAnchorEventTracker = [];
 jQuery("a.product_type_external, .single_add_to_cart_button").on("click", function(event){
     externalAnchorEventTracker.push(event.type);
@@ -368,7 +370,6 @@ jQuery(".product_thumbnail > a").on("click", function(event){
 	} else {
 		productThumbnailEventTracker = [];
 	}
-	
      }
    });
  });
@@ -378,7 +379,11 @@ if (jQuery(".easyzoom").length ) {
 		// Nothing to do
 	} else {
   setTimeout(function() {
-		jQuery(".easyzoom").prepend('<a href='+jQuery("a.social_media_pinterest").attr("href")+' target="_blank" style="display: inline-block;position: absolute;bottom: 10px;left: 10px;padding:0 10px;color: #CC1A1C;font-size: 50px;z-index:100;"><i class="fa fa-pinterest"></i></a>');
+		jQuery(".easyzoom").prepend('<a href='+jQuery("a.social_media_pinterest").attr("href")+' target="_blank" style="display: inline-block;position: absolute;bottom: 10px;left: 10px;padding:0 10px;color: #CC1A1C;font-size: 50px;z-index:100;" class="social_pinit_btn"><i class="fa fa-pinterest"></i></a>');
+		jQuery("a.social_pinit_btn").on("click", function(event){
+			event.preventDefault();
+			window.location.href = jQuery(this).attr("href");
+		});
 },100)
 	}
 }
